a19be43c251db4ae1f080762883219f23e1a251a
liyanjun <1418701248@qq.com>
2024-01-08 22:58:52 +0800

    svg配置

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div>app</div>
   <div class="contain">
-    <el-button :icon="Check">啊啊啊啊啊</el-button>
-    <h1 class="box">{{ a }}</h1>
-    <el-table mb-1 :data="[]" />
-    <el-pagination :total="100" />
-    <img src="@/assets/images/eslint_rules.png" alt="" />
+    <svg>
+      <use xlink:href="#icon-remark"></use>
+    </svg>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-import { Check } from '@element-plus/icons-vue'
-let a = ref(0)
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
 .contain {
-  background-color: #9cea49;
+  // background-color: #9cea49;
   .box {
     width: 100px;
     height: 200px;
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // 引入element-plus
 import ElemenPlus from 'element-plus'
+// svg插件需要配置代码
+import 'virtual:svg-icons-register'
 import 'element-plus/dist/index.css'
-//@ts-expect-error '引入国际化报错是因为没有ts类型'
+//@ts-expect-error '引入国际化报错是因为没有ts类型，@ts-expect-error不去检验ts类型'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs' // 国际化
 const app = createApp(App)
 app.use(ElemenPlus, {
   locale: zhCn,
 }) // 应用
 app.mount('#app') // 挂载
+// console.log(import.meta.env) 打印环境变量
